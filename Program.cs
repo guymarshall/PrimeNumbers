6c789393bfb5fd26a767c8529d1834fd05b77abd
Guy Marshall <guy@cslgroupservices.co.uk>
2022-05-06 14:16:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿public class PrimeNumbers
+{
+    public static bool IsPrime(int number)
+    {
+        for (int i = 2; i <= Math.Ceiling(Math.Sqrt(number)); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Enter a positive integer: ");
+        int userInput = int.Parse(Console.ReadLine());
+        Console.WriteLine("Prime numbers:");
+
+        for (int i = 2; i < userInput; i++)
+        {
+            if (IsPrime(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
